d09435dcfd6641042fdb086bab94866f448a0ae0
Connor Pak <connorpak@Connors-MacBook-Pro.local>
2023-08-27 17:29:04 -0700

    pwetty pwease give me a hundred ><

--- src/main/java/u1pp/Main.java
+++ src/main/java/u1pp/Main.java
@@ -1,7 +1,6 @@
 package u1pp;
 
-public class Main 
-{
+public class Main {
 	/**
 	 * Main function, where the code starts running when you click "run"
 	 */
@@ -15,7 +14,7 @@ public class Main
      * returns the average of 3 integers. 
      * Should return a double that is mathematically accurate. 
      */
-    public static double averageOfThree(int a, int b, int c) {
+    public static double averageOfThree(double a, double b, double c) {
     	return (a + b + c) / 3;
     }
     
@@ -23,8 +22,8 @@ public class Main
      * Prints a 1-line greeting based on the name given
      */
     public static void printGreeting(String name) {
-    	System.out.println("hello");
-    	System.out.println(name);
+    	System.out.print("Hello ");
+    	System.out.print(name);
     }
     
 }
